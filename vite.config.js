fc9a6bbcd842225ba97ebb7e59605c73ef74b07a
Witchie <matin.witchiedev@gmail.com>
2024-08-09 19:29:42 +0430

    finished base design

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import laravel from "laravel-vite-plugin";
 import path from "path";
 
 export default defineConfig({
-  server:{
-    host: '0.0.0.0',
-    port: 5173,
-  },
   plugins: [
     laravel({
       input: ["resources/css/app.css", "resources/js/app.js"],
