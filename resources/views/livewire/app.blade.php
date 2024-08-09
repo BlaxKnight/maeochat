b929a7a068acd1e6ba63567360dae0987ca3033e
Witchie <matin.witchiedev@gmail.com>
2024-08-09 19:18:25 +0430

    a

--- resources/views/livewire/app.blade.php
+++ resources/views/livewire/app.blade.php
@@ -6,7 +6,8 @@
         class="flex flex-col justify-end relative grow shadow-inner bg-orange-50 before:content-[' '] before:bg-[url('/public/images/chat-bg.svg')] before:w-full before:h-full before:bg-no-repeat before:bg-center before:bg-cover before:absolute before:opacity-30 z-0 before:z-10">
         <ul class="relative z-20 p-3 flex flex-col gap-4 font-medium">
           <li class="flex items-end gap-1">
-            <div class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
+            <div
+              class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
               This is a normal message, which is limited to 70% of the device's width.
             </div>
             <div class="text-slate-800">
@@ -28,7 +29,8 @@
             </div>
           </li>
           <li class="flex items-end gap-1">
-            <div class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
+            <div
+              class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
               This message has not been sent yet...
             </div>
             <div class="text-slate-800">
@@ -39,7 +41,8 @@
             </div>
           </li>
           <li class="flex items-end gap-1">
-            <div class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
+            <div
+              class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
               This message is sent and seen.
             </div>
             <div class="text-slate-800">
@@ -62,7 +65,8 @@
             </div>
           </li>
           <li class="flex items-end gap-1">
-            <div class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
+            <div
+              class="bg-white border border-slate-300 py-3 px-4 rounded-ss-2xl rounded-e-2xl inline-block max-w-[65%]">
               <div class="text-sm text-slate-600 bg-orange-100 border-s-4 border-orange-300 mb-1 py-1 px-2">
                 This message is sent and seen.
               </div>
@@ -76,11 +80,23 @@
               <span class="m-1">12:31 pm</span>
             </div>
           </li>
+          <li class="flex items-end flex-row-reverse gap-2">
+            <div class="bg-orange-600 text-white py-3 px-4 rounded-se-2xl rounded-s-2xl inline-block max-w-[65%]">
+              An error occurred while sending this message.
+            </div>
+            <div class="text-slate-700">
+              <span class="relative">
+                <i class="fa-solid fa-brake-warning text-red-600"></i>
+              </span>
+              <span class="m-1">12:28 pm</span>
+            </div>
+          </li>
         </ul>
       </div>
       <div class="flex justify-between items-center pe-3 hover:shadow transition-all">
         <input type="text"
-          class="grow border-0 border-transparent focus:border-transparent focus:ring-0 caret-orange-700" placeholder="Send message...">
+          class="grow border-0 border-transparent focus:border-transparent focus:ring-0 caret-orange-700"
+          placeholder="Send message...">
         <i class="fa-solid fa-paper-plane-top fa-xl cursor-pointer text-orange-600"></i>
       </div>
     </section>
