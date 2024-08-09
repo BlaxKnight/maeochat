b85c71fd957e60399d59c419f88b29296e7881bb
Witchie <matin.witchiedev@gmail.com>
2024-08-09 16:36:11 +0430

    background image added

--- resources/views/livewire/app.blade.php
+++ resources/views/livewire/app.blade.php
@@ -1,8 +1,8 @@
 <main id="app" class="flex justify-center items-center h-screen">
   <div class="flex flex-col justify-between w-96 h-[90vh] border rounded-2xl">
     @include('partials.app.header')
-    <section id="chat" class="grow shadow-inner bg-slate-100"></section>
-    <section id="send-message">
+    <section class="grow flex flex-col justify-between">
+      <div class="grow shadow-inner bg-slate-100 bg-[url('/public/images/chat-bg.svg')] bg-no-repeat bg-center bg-cover"></div>
       <div class="flex justify-between items-center pe-3 hover:shadow transition-all">
         <input type="text"
           class="grow border-0 border-transparent focus:border-transparent focus:ring-0 caret-orange-700">
