1a8a9729eb8a8a46126cc68d4c258dd1b06f49a6
Witchie <matin.witchiedev@gmail.com>
2024-08-09 09:07:10 +0430

    fixed icon bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
-  @vite('resources/assets/fonts/sharp-regular.css')
+  @vite(['resources/css/app.css', 'resources/assets/icons/css/duotone.css', 'resources/assets/icons/css/fontawesome.css'])
 </head>
 
 <body>
@@ -13,14 +12,16 @@
     <div class="flex flex-col justify-between w-96 h-[90vh] border rounded-2xl">
       <header id="header" class="text-center border-b p-2">
         <h1 class="text-lg leading-4 font-medium text-orange-500">Meowchat</h1>
-        <div class="text-slate-800 text-3xl font-medium">This is a <span class="text-orange-700">test</span> build!</div>
+        <div class="text-slate-800 text-3xl font-medium">This is a <span class="text-orange-700">test</span> build!
+        </div>
         <div class="text-slate-600 text-sm">Some features are placeholders.</div>
       </header>
       <section id="chat" class="grow shadow-inner bg-slate-100"></section>
       <section id="send-message">
-        <div class="flex justify-between">
-          <input type="text" class="grow">
-
+        <div class="flex justify-between items-center pe-3">
+          <input type="text" class="grow border-0 outline-none">
+          <i class="fa-duotone fa-solid fa-paper-plane-top fa-2xl"
+            style="--fa-primary-color: rgb(241 245 249); --fa-secondary-color: rgb(249 115 22); --fa-secondary-opacity: 0.9"></i>
         </div>
       </section>
       <footer id="version" class="p-2 text-center border-t text-slate-700">
