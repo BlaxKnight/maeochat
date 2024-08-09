b3ef14b3306100f598af3b4b951b879dd766e5f9
Mohammad Amin Mirzaee <predator.blaxknight@gmail.com>
2024-08-09 19:20:59 +0330

    Fixed @vite

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>maeochat</title>
-<link rel="stylesheet" href="{{ asset('') }}">
+@vite(['resources/css/app.css', 'resources/assets/icons/css/solid.css', 'resources/assets/icons/css/fontawesome.css'])
+
