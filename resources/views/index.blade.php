6a70a25bf74f90192c0602150a5c0dd98396f59e
Mohammad Amin Mirzaee <predator.blaxknight@gmail.com>
2024-08-09 09:11:28 +0330

    pages updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>maeochat</title>
-  @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/assets/icons/css/duotone.css', 'resources/assets/icons/css/fontawesome.css'])
 </head>
 <body>
   @livewire('app')
--- resources/views/livewire/app.blade.php
+++ resources/views/livewire/app.blade.php
@@ -2,11 +2,18 @@
   <div class="flex flex-col justify-between w-96 h-[90vh] border rounded-2xl">
     <header id="header" class="text-center border-b p-2">
       <h1 class="text-lg leading-4 font-medium text-orange-500">Meowchat</h1>
-      <div class="text-slate-800 text-3xl font-medium">This is a <span class="text-orange-700">test</span> build!</div>
+      <div class="text-slate-800 text-3xl font-medium">This is a <span class="text-orange-700">test</span> build!
+      </div>
       <div class="text-slate-600 text-sm">Some features are placeholders.</div>
     </header>
-    <section id="chat" class="grow"></section>
-    <section id="send-message"></section>
+    <section id="chat" class="grow shadow-inner bg-slate-100"></section>
+    <section id="send-message">
+      <div class="flex justify-between items-center pe-3">
+        <input type="text" class="grow border-0 outline-none">
+        <i class="fa-duotone fa-solid fa-paper-plane-top fa-2xl"
+          style="--fa-primary-color: rgb(241 245 249); --fa-secondary-color: rgb(249 115 22); --fa-secondary-opacity: 0.9"></i>
+      </div>
+    </section>
     <footer id="version" class="p-2 text-center border-t text-slate-700">
       <a href="https://github.com/BlaxKnight/maeochat" class=" font-bold hover:text-orange-700 transition-colors">
         <div class="flex justify-center gap-2">
