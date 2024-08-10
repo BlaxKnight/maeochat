9e204cede51feabf00a9099cf15f17c5ffb4ab75
Mohammad Amin Mirzaee <predator.blaxknight@gmail.com>
2024-08-10 07:14:12 +0330

    Vite Config Changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from "laravel-vite-plugin";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+  },
   plugins: [
     laravel({
       input: ["resources/css/app.css", "resources/js/app.js"],
